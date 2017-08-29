4983a09854fbf24770f312b7fc973df288bda9cc
Chaichalit Srisawat <boywert@gmail.com>
2017-08-29 15:06:24 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -13,38 +13,35 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully\n";
 
-// $mysqli = new mysqli("192.168.2.48", "ViMala", "ViMala@Sql", "ViMala");
+
 $stmt = $conn->prepare("SELECT ID,TIME,STATUS FROM JobSubmission WHERE USER = ?;");
 $stmt->bind_param("i", $user_id);
 if ($stmt->execute())
     echo "executed successfully\n";
   
-// $content = "<b>Username:</b>&nbsp;&nbsp;&nbsp;&nbsp<br><br>\n";
-
 $result = $stmt->get_result();
 print_r($result);
-// echo $result;
-// if ($result->num_rows > 0) {
-//     $content = $content . "<table><tr><th>Job ID</th><th>Time added</th><th>Status</th></tr>";
-//     echo $content;
-//     while ($row = $result->fetch_assoc()) {
-// 	if ($row['STATUS'] == 0 )
-// 	    $status = "Queueing";
-// 	if ($row['STATUS'] == 1 )
-// 	    $status = "Completed";
-// 	if ($row['STATUS'] == 2 )
-// 	    $status = "Cancelled by user";
-// 	if ($row['STATUS'] == 3 )
-// 	    $status = "Cancelled by system";
-// 	$content = $content . "<tr><td>".$row["ID"]."</td><td>".$row["TIME"]."</td><td>".$status."</td></tr>";
-//     }
-//     $content = $content . "</table>";
-// } else {
-//     $content = $content . "There is no job submitted.";
-// }
 
-// $result->free();
-
-// $stmt->close();
-// $mysqli->close();
+if ($result->num_rows > 0) {
+    $content = $content . "<table><tr><th>Job ID</th><th>Time added</th><th>Status</th></tr>";
+    echo $content;
+    while ($row = $result->fetch_assoc()) {
+	if ($row['STATUS'] == 0 )
+	    $status = "Queueing";
+	if ($row['STATUS'] == 1 )
+	    $status = "Completed";
+	if ($row['STATUS'] == 2 )
+	    $status = "Cancelled by user";
+	if ($row['STATUS'] == 3 )
+	    $status = "Cancelled by system";
+	$content = $content . "<tr><td>".$row["ID"]."</td><td>".$row["TIME"]."</td><td>".$status."</td></tr>";
+    }
+    $content = $content . "</table>";
+} else {
+    $content = $content . "There is no job submitted.";
+}
+echo $content;
+$result->free();
+$stmt->close();
+$conn->close();
 ?>
