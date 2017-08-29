9444ffeff2a12d4d0288006a47359792571df5f6
Chaichalit Srisawat <boywert@gmail.com>
2017-08-29 09:46:18 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -10,7 +10,7 @@ if (!($stmt->execute()))
 $content = "<b>Username:</b>&nbsp;&nbsp;&nbsp;&nbsp<br><br>\n";
 
 $result = $stmt->get_result();
-
+print_r($result);
 if ($result->num_rows > 0) {
     $content = $content . "<table><tr><th>Job ID</th><th>Time added</th><th>Status</th></tr>";
     echo $content;
