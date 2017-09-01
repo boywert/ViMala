36fec7516f3528564eb8177bd5f36eb6945e58da
Chaichalit Srisawat <boywert@gmail.com>
2017-09-01 09:17:09 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -5,22 +5,22 @@ $password = "ViMala@Sql";
 $db_name = "ViMala";
 // Create connection
 $mysqli = new mysqli($servername, $username, $password, $db_name);
-
+echo "bla";
 // Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 echo "Connected successfully\n";
-if(!($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0"))) {
-    print "Failed to prepare statement\n";
-} else {
+if($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0")) {
     echo " test";
     if ($result->num_rows > 0) {
 	while ($row = $result->fetch_assoc()) {
 	    $field_array = explode($row['PARAMS'],"|");
 	    print_r($field_array);
 	}
-    $result->free();
-}
+       
+} else 
+    print "Failed to prepare statement\n";
+$result->free(); 
 $conn->close();
 ?>
