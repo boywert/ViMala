73cd651857935a8a7372bc3b03e2dd055d1fae92
Chaichalit Srisawat <boywert@gmail.com>
2017-09-01 09:19:30 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -6,7 +6,7 @@ $password = "ViMala@Sql";
 $db_name = "ViMala";
 // // Create connection
 $mysqli = new mysqli($servername, $username, $password, $db_name);
-// echo "bla";
+echo "bla";
 // // Check connection
 // if ($mysqli->connect_error) {
 //     die("Connection failed: " . $mysqli->connect_error);
