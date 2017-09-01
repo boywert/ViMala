5b08327d4706818550fb170d544c903f3bd2b54b
Chaichalit Srisawat <boywert@gmail.com>
2017-09-01 09:57:29 +0700

    quick commit

--- scripts/submission.py
+++ scripts/submission.py
@@ -4,7 +4,6 @@ def main():
     job_id = sys.argv[1]
     sql = sys.argv[2]
     print "Reading SQLite3 table"
-    start = timer()
     conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
     c = conn.cursor()
     c.execute(sql)
