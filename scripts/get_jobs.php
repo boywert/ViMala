512c2fd35de0adad52978f7f18c4b83c8cf59f91
Chaichalit Srisawat <boywert@gmail.com>
2017-09-01 09:07:25 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -12,29 +12,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully\n";
-if(!($result = $mysqli->query("SELECT ID,TIME,STATUS FROM JobSubmission WHERE STATUS = 0"))) {
+if(!($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0"))) {
     print "Failed to prepare statement\n";
 } else {
-    print_r($result);
-    echo $result->num_rows;
     if ($result->num_rows > 0) {
-	$content = $content . "<table><tr><th>Job ID</th><th>Time added</th><th>Status</th></tr>";
-	while ($row = $result->fetch_assoc()) {
-	    if ($row['STATUS'] == 0 )
-		$status = "Queueing";
-	    if ($row['STATUS'] == 1 )
-		$status = "Completed";
-	    if ($row['STATUS'] == 2 )
-		$status = "Cancelled by user";
-	    if ($row['STATUS'] == 3 )
-		$status = "Cancelled by system";
-	    $content = $content . "<tr><td>".$row["ID"]."</td><td>".$row["TIME"]."</td><td>".$status."</td></tr>\n";
+	while ($field_array = $result->fetch_assoc()) {
+	    $field = explode($row['PARAMS'],"|");
+	    print_r($field_array);
 	}
-	$content = $content . "</table>";
-    } else {
-	$content = $content . "There is no job submitted.";
-    }
-    echo $content;
     $result->free();
 }
 $stmt->close();
