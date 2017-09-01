752bb6debd1aa7388e79b14e13dddd7d4273242e
Chaichalit Srisawat <boywert@gmail.com>
2017-09-01 09:18:40 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -4,24 +4,24 @@ $servername = "192.168.2.48";
 $username = "ViMala";
 $password = "ViMala@Sql";
 $db_name = "ViMala";
-// Create connection
-$mysqli = new mysqli($servername, $username, $password, $db_name);
-echo "bla";
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-echo "Connected successfully\n";
-if($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0")) {
-    echo " test";
-    if ($result->num_rows > 0) {
-	while ($row = $result->fetch_assoc()) {
-	    $field_array = explode($row['PARAMS'],"|");
-	    print_r($field_array);
-	}
+// // Create connection
+// $mysqli = new mysqli($servername, $username, $password, $db_name);
+// echo "bla";
+// // Check connection
+// if ($mysqli->connect_error) {
+//     die("Connection failed: " . $mysqli->connect_error);
+// }
+// echo "Connected successfully\n";
+// if($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0")) {
+//     echo " test";
+//     if ($result->num_rows > 0) {
+// 	while ($row = $result->fetch_assoc()) {
+// 	    $field_array = explode($row['PARAMS'],"|");
+// 	    print_r($field_array);
+// 	}
        
-} else 
-    print "Failed to prepare statement\n";
-$result->free(); 
-$mysqli->close();
+// } else 
+//     print "Failed to prepare statement\n";
+// $result->free(); 
+// $mysqli->close();
 ?>
