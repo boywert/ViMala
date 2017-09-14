2039a3d0f5795510dc44a0c4fb068f0f4eac055e
Chaichalit Srisawat <boywert@gmail.com>
2017-09-14 08:54:40 +0700

    quick commit

--- scripts/submission.py
+++ scripts/submission.py
@@ -13,7 +13,7 @@ def main():
     coor = numpy.array(result)
     fig = plt.figure()
     ax = fig.add_subplot(111, polar=True)
-    ax.scatter(coor[:,0],coor[:,1]/numpy.pi*180,s=0.1)
+    ax.scatter(coor[:,0],coor[:,1]/numpy.pi*180,s=0.1,marker=".")
     ax.grid(True)
     fig.savefig("lightcone.png")
     return 0
