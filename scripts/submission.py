34b6ef3a9305099f861d9343079028d8b0e1e011
Chaichalit Srisawat <boywert@gmail.com>
2017-09-15 04:05:05 +0700

    quick commit

--- scripts/submission.py
+++ scripts/submission.py
@@ -15,6 +15,8 @@ def main():
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
+    for r in coor:
+        print r
     fig = plt.figure(figsize=(8, 6),dpi=240)
     ax = fig.add_subplot(111, polar=True)
     ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".")
