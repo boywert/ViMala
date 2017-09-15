95f8d91730c57a7d9795fbcf1c9f10e3af3cbbf2
Chaichalit Srisawat <boywert@gmail.com>
2017-09-15 19:22:21 +0700

    quick commit

--- scripts/Lightcone.php
+++ scripts/Lightcone.php
@@ -8,7 +8,7 @@ f21cm  = 1420.4057517667
 def main():
     print "Reading SQLite3 table"
     fig = plt.figure(figsize=(8, 6),dpi=240)
-    ax = fig.add_subplot(111, polar=True)
+    ax1 = fig.add_subplot(111, polar=True)
     conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
     c = conn.cursor()
     
@@ -19,29 +19,31 @@ def main():
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
-    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="g",alpha=0.1)
+    ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="g",alpha=0.1)
     sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
-    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.2,marker=".",color="k",alpha=0.4)
+    ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.2,marker=".",color="k",alpha=0.4)
     sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
-    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.4,marker=".",color="b",alpha=0.7)
+    ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.4,marker=".",color="b",alpha=0.7)
     sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
-    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.6,marker=".",color="r")
+    ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.6,marker=".",color="r")
     
-    ax.grid(True)
-    ax.set_ylim(0.0,0.16)
-    ax.set_rlabel_position(0)
+    ax1.grid(True)
+    ax1.set_ylim(0.0,0.16)
+    ax1.set_rlabel_position(0)
+
+    
     fig.savefig("lightcone.png")
     return 0
 if __name__ == "__main__":
