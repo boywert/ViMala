04a9277a8cc5ddac72604498c73d6ba5f0122879
Chaichalit Srisawat <boywert@gmail.com>
2017-09-15 18:31:59 +0700

    quick commit

--- scripts/Lightcone.php
+++ scripts/Lightcone.php
@@ -7,20 +7,35 @@ import matplotlib.pyplot as plt
 f21cm  = 1420.4057517667
 def main():
     print "Reading SQLite3 table"
+    fig = plt.figure(figsize=(8, 6),dpi=240)
+    ax = fig.add_subplot(111, polar=True)
     conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
     c = conn.cursor()
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux > 1700 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
-    print numpy.histogram(coor[:,0]/numpy.pi*180,bins=180)
-    print numpy.histogram(coor[:,1],bins=18)
-    #for a in coor:
-    #    print a
-    fig = plt.figure(figsize=(8, 6),dpi=240)
-    ax = fig.add_subplot(111, polar=True)
     ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="r")
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="g")
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="b")
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 170 AND Flux >= 50 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="b")
+    
     ax.grid(True)
     ax.set_ylim(0.0,0.16)
     ax.set_rlabel_position(0)
