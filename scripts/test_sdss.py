83e507f8b4a055056e2630724819c4e64fa310c1
Chaichalit Srisawat <boywert@gmail.com>
2017-09-16 19:51:36 +0700

    quick commit

--- scripts/test_sdss.py
+++ scripts/test_sdss.py
@@ -20,7 +20,7 @@ def main():
     #    print a
     fig = plt.figure(figsize=(8, 6),dpi=240)
     ax = fig.add_subplot(111, projection='polar')
-    ax.scatter(coor[:,0]/numpy.pi*180,coor[:,1],s=0.1,marker=".")
+    ax.scatter(coor[:,0],coor[:,1],s=0.1,marker=".")
     ax.grid(True)
     ax.set_ylim(0.0,0.16)
     ax.set_rlabel_position(0)
