075803ca3cba788cb404618db3fc59bbfce3b5b8
Chaichalit Srisawat <boywert@gmail.com>
2017-09-16 15:46:12 +0700

    quick commit

--- scripts/Lightcone.php
+++ scripts/Lightcone.php
@@ -13,27 +13,27 @@ def main():
 
     ax1 = fig.add_subplot(121, polar=True)
     
-
+    angle = 20.0
     
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 170 AND Flux >= 50 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 170 AND Flux >= 50 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="g",alpha=0.1)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.2,marker=".",color="k",alpha=0.4)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax1.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.4,marker=".",color="b",alpha=0.7)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
@@ -44,27 +44,27 @@ def main():
     ax1.set_ylim(0.0,0.16)
     ax1.set_rlabel_position(0)
 
-
+    
     ax2 = fig.add_subplot(122, polar=True)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.70e-4 AND FluxDensity >= 5.0e-5 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.70e-4 AND FluxDensity >= 5.0e-5 AND PosTheta > 0 AND PosTheta < angle*(3.142/1angle))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax2.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".",color="g",alpha=0.1)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 5.2e-4 AND FluxDensity >= 1.70e-4 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 5.2e-4 AND FluxDensity >= 1.70e-4 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax2.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.2,marker=".",color="k",alpha=0.4)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.7e-3 AND FluxDensity >=5.40e-4 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.7e-3 AND FluxDensity >=5.40e-4 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
     print len(coor)
     ax2.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.4,marker=".",color="b",alpha=0.7)
-    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity >= 1.7e-3 AND PosTheta > 0 AND PosTheta < 80.0*(3.142/180.0))"
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity >= 1.7e-3 AND PosTheta > 0 AND PosTheta < angle*(3.142/180))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
