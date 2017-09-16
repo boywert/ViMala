c74e6fa37c67d879c4a6af6cff4c86bd09bee8c9
Chaichalit Srisawat <boywert@gmail.com>
2017-09-16 19:33:06 +0700

    quick commit

--- scripts/test_sdss.py
+++ scripts/test_sdss.py
@@ -9,7 +9,7 @@ def main():
     print "Reading SQLite3 table"
     conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
     c = conn.cursor()
-    sql = "SELECT PosTheta,Redshift FROM Lightcone WHERE (PosTheta < 3.142/2 AND StellarMass > 1e8 AND (PosPhi > 0.0*(3.142/180.0) AND PosPhi < 30.0*(3.142/180.0)))"
+    sql = "SELECT PosTheta,Redshift FROM Lightcone WHERE ((PosTheta < 3.142/2 ) AND (StellarMass > 1e8) AND (PosPhi > 0.0*(3.142/180.0) AND PosPhi < 30.0*(3.142/180.0)))"
     cursor = c.execute(sql)
     result = cursor.fetchall()
     coor = numpy.array(result)
@@ -20,7 +20,7 @@ def main():
     #    print a
     fig = plt.figure(figsize=(8, 6),dpi=240)
     ax = fig.add_subplot(111, polar=True)
-    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".")
+    ax.scatter(coor[:,1]/,coor[:,0]/numpy.pi*180,s=0.1,marker=".")
     ax.grid(True)
     ax.set_ylim(0.0,0.16)
     ax.set_rlabel_position(0)
