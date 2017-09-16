8c0c5e9d1add40153e45a4c226d7da391d79fd16
Chaichalit Srisawat <boywert@gmail.com>
2017-09-16 16:27:02 +0700

    quick commit

---
+++ scripts/test_sdss.py
@@ -0,0 +1,32 @@
+import sqlite3
+import sys
+import numpy
+import matplotlib
+matplotlib.rcParams.update({'font.size': 6})
+import matplotlib.pyplot as plt
+f21cm  = 1420.4057517667
+def main():
+    job_id = sys.argv[1]
+    sql = sys.argv[2]
+    print "Reading SQLite3 table"
+    conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
+    c = conn.cursor()
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (StellarMass > 1e8 AND PosTheta > 0 AND PosTheta < 20.0*(3.142/180.0))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    print numpy.histogram(coor[:,0]/numpy.pi*180,bins=180)
+    print numpy.histogram(coor[:,1],bins=18)
+    #for a in coor:
+    #    print a
+    fig = plt.figure(figsize=(8, 6),dpi=240)
+    ax = fig.add_subplot(111, polar=True)
+    ax.scatter(coor[:,1]/numpy.pi*180,coor[:,0],s=0.1,marker=".")
+    ax.grid(True)
+    ax.set_ylim(0.0,0.16)
+    ax.set_rlabel_position(0)
+    fig.savefig("lightcone.png")
+    return 0
+if __name__ == "__main__":
+    main()
