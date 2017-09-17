3dc1b533aa8a8eccbc7ef472021fa32e8b4b51c1
Chaichalit Srisawat <boywert@gmail.com>
2017-09-17 11:59:54 +0700

    quick commit

--- scripts/Lightcone.php
+++ scripts/Lightcone.php
@@ -9,76 +9,76 @@ def main():
     print "Reading SQLite3 table"
     conn = sqlite3.connect('/share/data2/VIMALA/Lightcone/example.db')
     c = conn.cursor()
-    # fig = plt.figure(figsize=(16, 8),dpi=240)
+    fig = plt.figure(figsize=(16, 8),dpi=240)
 
-    # ax1 = fig.add_subplot(121, polar=True)
+    ax1 = fig.add_subplot(121, polar=True)
     
-    # angle = 20.0
+    angle = 20.0
     
     
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 170 AND Flux >= 50 AND (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax1.scatter(coor[:,0],coor[:,1],s=0.1,marker=".",color="g",alpha=0.1)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax1.scatter(coor[:,0],coor[:,1],s=0.2,marker=".",color="k",alpha=0.4)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax1.scatter(coor[:,0],coor[:,1],s=0.4,marker=".",color="b",alpha=0.7)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax1.scatter(coor[:,0],coor[:,1],s=0.6,marker=".",color="r")
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 170 AND Flux >= 50 AND (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax1.scatter(coor[:,0],coor[:,1],s=0.01,marker=".",color="g",alpha=0.1)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 520 AND Flux >= 170 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax1.scatter(coor[:,0],coor[:,1],s=0.02,marker=".",color="k",alpha=0.4)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux < 1700 AND Flux >=540 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax1.scatter(coor[:,0],coor[:,1],s=0.04,marker=".",color="b",alpha=0.7)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (Flux >= 1700 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax1.scatter(coor[:,0],coor[:,1],s=0.1,marker=".",color="r")
     
-    # ax1.grid(True)
-    # ax1.set_ylim(0.0,0.16)
-    # ax1.set_rlabel_position(0)
-
-    
-    # ax2 = fig.add_subplot(122, polar=True)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.70e-4 AND FluxDensity >= 5.0e-5 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax2.scatter(coor[:,0],coor[:,1],s=0.1,marker=".",color="g",alpha=0.1)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 5.2e-4 AND FluxDensity >= 1.70e-4 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax2.scatter(coor[:,0],coor[:,1],s=0.2,marker=".",color="k",alpha=0.4)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.7e-3 AND FluxDensity >=5.40e-4 AND (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax2.scatter(coor[:,0],coor[:,1],s=0.4,marker=".",color="b",alpha=0.7)
-    # sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity >= 1.7e-3 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
-    # cursor = c.execute(sql)
-    # result = cursor.fetchall()
-    # coor = numpy.array(result)
-    # print len(coor)
-    # ax2.scatter(coor[:,0],coor[:,1],s=0.6,marker=".",color="r")
-    
-    # ax2.grid(True)
-    # ax2.set_ylim(0.0,0.16)
-    # ax2.set_rlabel_position(0)
+    ax1.grid(True)
+    ax1.set_ylim(0.0,0.16)
+    ax1.set_rlabel_position(0)
 
     
-    # fig.savefig("lightcone.png")
-    # close(fig)
+    ax2 = fig.add_subplot(122, polar=True)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.70e-4 AND FluxDensity >= 5.0e-5 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax2.scatter(coor[:,0],coor[:,1],s=0.01,marker=".",color="g",alpha=0.1)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 5.2e-4 AND FluxDensity >= 1.70e-4 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax2.scatter(coor[:,0],coor[:,1],s=0.02,marker=".",color="k",alpha=0.4)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity < 1.7e-3 AND FluxDensity >=5.40e-4 AND (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax2.scatter(coor[:,0],coor[:,1],s=0.04,marker=".",color="b",alpha=0.7)
+    sql = "SELECT PosPhi,Redshift FROM Lightcone WHERE (FluxDensity >= 1.7e-3 AND  (PosTheta > 70.0*(3.142/180.0) AND PosTheta < 80.0*(3.142/180.0)))"
+    cursor = c.execute(sql)
+    result = cursor.fetchall()
+    coor = numpy.array(result)
+    print len(coor)
+    ax2.scatter(coor[:,0],coor[:,1],s=0.1,marker=".",color="r")
+    
+    ax2.grid(True)
+    ax2.set_ylim(0.0,0.16)
+    ax2.set_rlabel_position(0)
+
+    
+    fig.savefig("lightcone.png")
+    close(fig)
     fig = plt.figure()
 
     ax1 = fig.add_subplot(111)
