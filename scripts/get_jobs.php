777289361c1306be2f7be1e4ab679dfcc51ba1da
Chaichalit Srisawat <boywert@gmail.com>
2017-09-22 02:39:24 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -15,16 +15,18 @@ if ($mysqli->connect_error) {
 if($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0")) {
     //    echo " test\n";
     if ($result->num_rows > 0) { 
-	echo "more than 0\n";
- 	while ($row = $result->fetch_assoc()) {
-	    $row['PARAMS'][0] = "";
- 	    $field_array = explode("|",$row['PARAMS']);
- 	    unset($field_array[count($field_array)-1]);
-	    $string = implode(",", $field_array);
-	    $conds = $mysqli->real_escape_string($row['CONDITIONS']);
-	    $sql = "SELECT ". $string . " FROM Lightcone WHERE ". $conds;
-	    echo $sql;
- 	}
+        echo "more than 0\n";
+        while ($row = $result->fetch_assoc()) {
+            $row['PARAMS'][0] = "";
+            $field_array = explode("|",$row['PARAMS']);
+            unset($field_array[count($field_array)-1]);
+            $string = implode(",", $field_array);
+            $conds = $mysqli->real_escape_string($row['CONDITIONS']);
+            $sql = "SELECT ". $string . " FROM Lightcone";
+            if($conds != "")
+                $sql .= "WHERE ". $conds;
+            echo $sql;
+        }
     }
 }
 // } else 
