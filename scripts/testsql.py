bf65194b95bf781ac737985d617a908f07e832cf
Chaichalit Srisawat <boywert@gmail.com>
2017-09-22 01:46:55 +0700

    quick commit

--- scripts/testsql.py
+++ scripts/testsql.py
@@ -1,12 +1,24 @@
 import mysql.connector
-cnx = mysql.connector.connect(host="192.168.2.48",user='ViMala',password="ViMala@Sql", database='ViMala')
+import sys
+sys.path.append("../")
+sys.path.append("../lib/")
+sys.path.append("../python/")
+from config import *
 
-sql = "CREATE TABLE IF NOT EXISTS `ViMala`.`DBSTRUCT` ( `ID` INT NOT NULL , `FIELD` TEXT NOT NULL , `DESCRIPTION` TEXT NOT NULL , `TYPE` TEXT NOT NULL ) ENGINE = InnoDB;"
-cur = cnx.cursor()
-cur.execute(sql)
-cnx.commit()
-add_field = ("INSERT INTO `ViMala`.`DBSTRUCT` (ID, FIELD, DESCRIPTION, TYPE) VALUES (1, %s, %s, %s)") 
-field_data = ("StellarMass","Stellar Mass in 1e10 Msun","<f4")
-cur.execute(add_field,field_data)
-cnx.commit()
-cnx.close()
+def main():
+    cnx = mysql.connector.connect(host="192.168.2.48",user='ViMala',password="ViMala@Sql", database='ViMala')
+    sql = "CREATE TABLE IF NOT EXISTS `ViMala`.`DBSTRUCT` ( `ID` INT NOT NULL , `FIELD` TEXT NOT NULL , `DESCRIPTION` TEXT NOT NULL , `TYPE` TEXT NOT NULL ) ENGINE = InnoDB;"
+    cur = cnx.cursor()
+    cur.execute(sql)
+    cnx.commit()
+    i = 0
+    for field in db_struct.names:
+        add_field = ("INSERT INTO `ViMala`.`DBSTRUCT` (ID, FIELD, DESCRIPTION, TYPE) VALUES ("+str(i)+",%s , %s, %s)") 
+        field_data = (field,"Description",db_struct[field])
+        cur.execute(add_field,field_data)
+    cnx.commit()
+    cnx.close()
+    return 0
+
+if __name__ == "__main__":
+    main()
