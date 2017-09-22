896cde73d5726359e0184d4a95f9bb8814c05e2a
Chaichalit Srisawat <boywert@gmail.com>
2017-09-22 10:45:55 +0700

    quick commit

--- scripts/get_jobs.php
+++ scripts/get_jobs.php
@@ -22,7 +22,7 @@ if($result = $mysqli->query("SELECT * FROM JobSubmission WHERE STATUS = 0")) {
             $field_array = explode("|",$row['PARAMS']);
             unset($field_array[count($field_array)-1]);
             $string = implode(",", $field_array);
-            $string = "*"
+            $string = "*";
             $conds = $mysqli->real_escape_string($row['CONDITIONS']);
             $sql = "SELECT ". $string . " FROM Lightcone";
             if($conds != "")
